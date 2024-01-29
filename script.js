873cd46f202b9ff0cbc0281805eb56317f9a15cf
peikra <mikheil.peikrishvili.1@btu.edu.ge>
2024-01-29 15:07:05 +0400

    exercise

---
+++ script.js
@@ -0,0 +1,54 @@
+
+//1
+// const calculate = document.getElementById("MyForm");
+
+// calculate.addEventListener("submit", function(event) {
+//     event.preventDefault()
+//     var len = document.getElementById("length")
+//     var width = document.getElementById("width")
+//     var result = document.querySelector("h2")
+//     var area = parseFloat(len.value )* parseFloat(width.value)
+//     result.innerHTML = area
+//     return result
+
+// });
+
+
+
+//2
+// function getSize(){
+//     let w = window.outerWidth;
+//     let h = window.outerHeight;
+//     let text = "width: " + w + "height: "+h;
+//     let wh = document.getElementById("wh");
+//     let txt = document.querySelector("h1");
+//     wh.append(txt);
+//     txt.innerHTML = text
+//     return txt
+// }
+
+
+
+
+//3
+// const submit = document.getElementById("form1").addEventListener('submit', function getFormvalue(event){
+//     event.preventDefault()
+//     var name = document.getElementById("first")
+//     var last_name = document.getElementById("second")
+//     var text = "First Name : "+ name.value + ",Last Name: " + last_name.value
+//     var result = document.querySelector(".result")
+//     result.innerHTML = text
+//     return result
+
+// })
+
+
+
+
+//4
+// const button = document.getElementById("button").addEventListener("click", function getOptions() {
+//     const select = document.getElementById("mySelect").value
+//     return alert(select)
+// })
+
+
